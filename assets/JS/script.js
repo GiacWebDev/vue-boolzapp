d3a7e719370c133f5acd0760063fb31863f26283
GiacWebDev <giacobechini@gmail.com>
2023-10-03 17:51:33 +0200

    [edit] sendmessage to fix

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -11,21 +11,36 @@ createApp({
       contacts,
       activeThumb: 0,
       userText: '',
+      userMessages: [], // Aggiungo array per memorizzare i messaggi dell'utente
 
      };
   },
 
   methods: {
-    //seleziono la thumb
-    selectThumb(index) {
-  
-        //svuoto l'input quando cambio chat
-        this.userText = '';
-  
-        this.activeThumb = index;
-
-        console.log(this.activeThumb);
+    // seleziono la thumb
+    selectThumb(index) { 
+      // svuoto l'input quando cambio chat
+      this.userText = '';
+      this.activeThumb = index;
+      console.log(this.activeThumb);
     },
-  },
+  
+    sendMessage() {
+      if (this.userText.trim() === '') {
+        return; // Non invio messaggi vuoti
+      }
+      // Creo un oggetto messaggio ed inserisco nel nuovo array userMessages
+      const userMessage = {
+        message: this.userText,
+        // Imposto il messaggio mandato con 'sent'
+        status: 'sent', 
+      };
+      console.log(userMessage); // Aggiungi questo console.log
 
+    }
+    
+    
+  },
+  
+  
 }).mount("#app");
