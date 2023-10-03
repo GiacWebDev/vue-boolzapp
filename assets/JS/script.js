7785975a44e308300415de4251baddb7ac39fe2b
GiacWebDev <giacobechini@gmail.com>
2023-10-03 16:12:19 +0200

    [edit] @click select Thumb

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -3,10 +3,29 @@ const { createApp } = Vue;
 import { contacts } from "./contacts.js";
 
 createApp({
+
   data() {
+
     return {
+
       contacts,
       activeThumb: 0,
+      userText: '',
+
      };
   },
+
+  methods: {
+    //seleziono la thumb
+    selectThumb(index) {
+  
+        //svuoto l'input quando cambio chat
+        this.userText = '';
+  
+        this.activeThumb = index;
+
+        console.log(this.activeThumb);
+    },
+  },
+
 }).mount("#app");
