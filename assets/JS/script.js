5d7204846ed63f623db112cf92f18c27feba3acb
GiacWebDev <giacobechini@gmail.com>
2023-10-04 11:08:15 +0200

    [edit] to fix push userMessage [edit] date message

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -11,7 +11,7 @@ createApp({
       contacts,
       activeContact: 0,
       userText: '',
-      userMessages: [], // Aggiungo array per memorizzare i messaggi dell'utente
+      userMessage: [], // Aggiungo array per memorizzare i messaggi dell'utente
 
      };
   },
@@ -26,20 +26,20 @@ createApp({
     },
   
     sendMessage() {
-      if (this.userText.trim() === '') {
-        return; // Non invio messaggi vuoti
-      }
       // Creo un oggetto messaggio ed inserisco nel nuovo array userMessages
       const userMessage = {
         message: this.userText,
         // Imposto il messaggio mandato con 'sent'
         status: 'sent',
-
       };
 
       this.contacts[this.activeContact].messages.push(userMessage);
 
+      this.userMessage = ''; // Resetto scritta input    
+
+      console.log(userMessage);
     }
+
     
   },
   
